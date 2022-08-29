cc70c8e7e73bf52cf52c5e14273162b60f8b15fa
gv4383 <gregory.vargas18@gmail.com>
2022-08-29 10:26:00 -0500

    Initial Commit

---
+++ XealChallenge/XealChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  XealChallengeApp.swift
+//  XealChallenge
+//
+//  Created by Goyo Vargas on 8/29/22.
+//
+
+import SwiftUI
+
+@main
+struct XealChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
