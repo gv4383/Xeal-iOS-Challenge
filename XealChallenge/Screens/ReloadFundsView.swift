3099cf41c515ed576a4f4912ea039947a894e128
gv4383 <gregory.vargas18@gmail.com>
2022-09-05 16:29:05 -0500

    Cleanup pt 2

--- XealChallenge/Screens/ReloadFundsView.swift
+++ XealChallenge/Screens/ReloadFundsView.swift
@@ -33,7 +33,7 @@ struct ReloadFundsView: View {
             return
         }
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
             isPayNowButtonLoading = false
         }
         
